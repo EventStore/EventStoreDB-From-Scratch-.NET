ff26b902536f17bab07ebbca719198aff08239b0
Stephen Tung <stktung@yahoo.com>
2024-10-09 23:37:55 +0800

    Split the sample into two. One for append and one for read.

---
+++ SampleAppend/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using EventStore.Client;
+
+var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
+await using var client = new EventStoreClient(settings);
+
+var eventType = "SampleEventType";
+var eventData = new EventData(
+    Uuid.NewUuid(),
+    eventType,
+    "{\"id\": \"1\", \"value\": \"some value\"}"u8.ToArray() // Fixed JSON format
+);
+
+var eventStream = "SampleStream";
+await client.AppendToStreamAsync(
+    eventStream,
+    StreamState.Any,
+    new List<EventData> { eventData }
+);
+
+Console.WriteLine("************************");
+Console.WriteLine("🎉 Congratulations, you have written an event!");
+Console.WriteLine("Stream: " + eventStream);
+Console.WriteLine("Event Type: " + eventType);
+Console.WriteLine("Event Body: " + Encoding.UTF8.GetString(eventData.Data.ToArray()));
+Console.WriteLine("************************");
---
+++ SampleRead/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using EventStore.Client;
+
+var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
+await using var client = new EventStoreClient(settings);
+	
+var events = client.ReadStreamAsync(
+  Direction.Forwards,
+  "SampleStream",
+  StreamPosition.Start
+);
+
+await foreach (var evt in events) {
+  Console.WriteLine("************************");
+  Console.WriteLine("You have read an event!");
+  Console.WriteLine("Stream: " + evt.Event.EventStreamId);
+  Console.WriteLine("Event Type: " + evt.Event.EventType);
+  Console.WriteLine("Event Body: " + Encoding.UTF8.GetString(evt.Event.Data.ToArray()));
+  Console.WriteLine("************************");
+}

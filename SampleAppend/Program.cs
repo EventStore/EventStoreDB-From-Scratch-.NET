a60ad13011f329ce3a2836a865f6503958a5b219
Stephen Tung <stktung@yahoo.com>
2024-10-23 16:50:36 +0800

    Added and polished some comments and code

--- SampleAppend/Program.cs
+++ SampleAppend/Program.cs
@@ -1,22 +1,47 @@
 ﻿using System.Text;
 using EventStore.Client;
 
+////////////////////////////////////////////////////////
+//
+// Step 1. Create client and connect it to EventStoreDB
+//
+////////////////////////////////////////////////////////
+
+// Create an instance of EventStoreDBClient, connecting to the EventStoreDB at localhost without TLS
 var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
 await using var client = new EventStoreClient(settings);
 
-var eventType = "SampleEventType";
-var eventData = new EventData(
-    Uuid.NewUuid(),
-    eventType,
-    "{\"id\": \"1\", \"value\": \"some value\"}"u8.ToArray() // Fixed JSON format
+/////////////////////////////////////////////////////////////
+//
+// Step 2. Create new event object with a type and data body
+//
+/////////////////////////////////////////////////////////////
+
+var eventType = "SampleEventType";                              // Define the event type for the new event
+var eventData = new EventData(                                  // Create a new event with a type and body
+    Uuid.NewUuid(),                                             // Specify a new UUID for the event
+    eventType,                                                  // Specify the event type
+    @"{""id"": ""1"", ""value"": ""some value""}"u8.ToArray()   // Specify the event data body as a JSON in byte format
 );
 
-var eventStream = "SampleStream";
-await client.AppendToStreamAsync(
-    eventStream,
-    StreamState.Any,
-    new List<EventData> { eventData }
+///////////////////////////////////////////////////
+//
+// Step 3. Append the event object into the stream
+//
+///////////////////////////////////////////////////
+
+var eventStream = "SampleStream";  // Define the stream name where the event will be appended
+await client.AppendToStreamAsync(  // Append the event to a stream
+    eventStream,                   // Name of the stream to append the event to       
+    StreamState.Any,               // Set to append regardless of the current stream state (you can ignore this for now)        
+    [eventData]                    // The event to append (in a list)
 );
+
+///////////////////////////////////////////////
+//
+// Step 4. Print the appended event to console
+//
+///////////////////////////////////////////////
 
 Console.WriteLine("************************");
 Console.WriteLine("🎉 Congratulations, you have written an event!");
--- SampleRead/Program.cs
+++ SampleRead/Program.cs
@@ -1,20 +1,41 @@
 ﻿using System.Text;
 using EventStore.Client;
 
+
+////////////////////////////////////////////////////////
+//
+// Step 1. Create client and connect it to EventStoreDB
+//
+////////////////////////////////////////////////////////
+
+// Create an instance of EventStoreDBClient, connecting to the EventStoreDB at localhost without TLS
 var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
 await using var client = new EventStoreClient(settings);
-	
-var events = client.ReadStreamAsync(
-  Direction.Forwards,
-  "SampleStream",
-  StreamPosition.Start
+
+///////////////////////////////////////////
+//
+// Step 2. Read all events from the stream
+//
+///////////////////////////////////////////
+
+var events = client.ReadStreamAsync(  // Read events from stream
+  Direction.Forwards,                 // Read events forward in time
+  "SampleStream",                     // Name of stream to read from
+  StreamPosition.Start                // Read from the start of the stream
 );
 
-await foreach (var evt in events) {
-  Console.WriteLine("************************");
-  Console.WriteLine("You have read an event!");
-  Console.WriteLine("Stream: " + evt.Event.EventStreamId);
-  Console.WriteLine("Event Type: " + evt.Event.EventType);
-  Console.WriteLine("Event Body: " + Encoding.UTF8.GetString(evt.Event.Data.ToArray()));
+///////////////////////////////////////
+//
+// Step 3. Print each event to console
+//
+///////////////////////////////////////
+
+await foreach (var evt in events) {                                   // For each event in stream
+  Console.WriteLine("************************");                      //
+  Console.WriteLine("You have read an event!");                       //
+  Console.WriteLine("Stream: " + evt.Event.EventStreamId);            // Print the stream name of the event
+  Console.WriteLine("Event Type: " + evt.Event.EventType);            // Print the type of the event
+  Console.WriteLine("Event Body: " + Encoding.UTF8.GetString(         // Print the body of the event. convert the byte array to string
+                                        evt.Event.Data.ToArray()));   // Gets the event body as a byte array
   Console.WriteLine("************************");
 }
